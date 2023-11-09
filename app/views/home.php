ddff57527a3a2dd2b9717be6fd2dd1a9813df0de
Marcos Del Carmen <marcosdelcarmen768@gmail.com>
2023-11-09 16:45:08 -0600

    login terminado

---
+++ app/views/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../../public/css/estilos.css">
+</head>
+<body>
+    <div class="cont_home">
+        <div class="cont_title">
+            <h3 class="title">Bienvenidos</h3>
+        </div>
+        <div class="cont_icons">
+            <i class="fa-solid fa-money-bill-trend-up"></i>
+            <i class="fa-solid fa-chart-column"></i>
+            <i class="fa-solid fa-chart-line"></i>
+            <i class="fa-regular fa-building"></i>
+            <i class="fa-solid fa-file-excel"></i>
+        </div>
+    </div>
+    <div class="cont_menu">
+        <div class="cont_icon_home">
+            <div class="icon">
+                <i class="fa-solid fa-calculator"></i>
+            </div>
+            <div class="title"><h2>Calculadora Fiscal</h2></div>
+        </div>
+
+        <div class="cont_icon cont_icon_info">
+            <a href="#">
+                <i class="fa-regular fa-user"></i>
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-right-from-bracket"></i>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
